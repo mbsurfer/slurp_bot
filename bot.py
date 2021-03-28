844ae558535c253f3f8ee3114d7214652d8b678f
Ryan Neldner <ryan@wpromote.com>
2021-03-28 13:31:36 -0700

    added logging

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import json
 import asyncio
+import logging
 from urllib.parse import urlparse
 
 import discord
@@ -10,6 +11,14 @@ from quart import Quart, request
 from dotenv import load_dotenv
 from blizzardapi import BlizzardApi
 
+logging.basicConfig(
+    level=logging.INFO,
+    filename="bot.log",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filemode="w",
+)
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
@@ -17,22 +26,19 @@ GUILD = os.getenv("DISCORD_GUILD")
 BLIZZARD_CLIENT = os.getenv("BLIZZARD_CLIENT")
 BLIZZARD_SECRET = os.getenv("BLIZZARD_SECRET")
 
-
 app = Quart(__name__)
 
 client = discord.Client()
 blizzard_client = BlizzardApi(BLIZZARD_CLIENT, BLIZZARD_SECRET)
 
 
-def get_wow_character_image_from_url(url):
+async def get_wow_character_image_from_url(url):
     """Returns the avartar image of the given wow character url.
 
     url - https://worldofwarcraft.com/en-us/character/us/malganis/astrocamp
     """
     parsed_armory_url = urlparse(url)
-    print(f"parsed_armory_url: {parsed_armory_url}")
     path_parts = parsed_armory_url.path.strip("/").rsplit("/")
-    print(f"path_parts: {path_parts}")
 
     locale, obj, region, realm_slug, character_name = path_parts
 
@@ -49,18 +55,20 @@ def format_application_repsonse(title, descrption):
     return f"**__{title}__**\n{descrption}"
 
 
-def embed_application_response(data):
+async def embed_application_response(data):
 
     embed = discord.Embed(
         title=data["name"], url=data["armory"], description=data["server"]
     )
 
-    thumbnail_url = get_wow_character_image_from_url(url=data["armory"])
+    thumbnail_url = await get_wow_character_image_from_url(url=data["armory"])
     embed.set_thumbnail(url=thumbnail_url)
 
     embed.add_field(name="Class", value=data["class"], inline=True)
     embed.add_field(name="Spec", value=data["spec"], inline=True)
     embed.add_field(name="Covenant", value=data["covenant"], inline=True)
+
+    embed.add_field(name="Logs", value=data["logs"], inline=False)
 
     return embed
 
@@ -74,9 +82,9 @@ async def create_applicant_channel(guild, channel_name):
         channel = await guild.create_text_channel(
             channel_name, category=applicant_category
         )
-        print(f"New channel created: {channel}")
+        logging.info(f"New channel created: {channel}")
     else:
-        print(f"Channel already exists: {channel}")
+        logging.warning(f"Channel already exists: {channel}")
 
     return channel
 
@@ -90,6 +98,7 @@ async def before_serving():
     loop = asyncio.get_event_loop()
     await client.login(TOKEN)
     loop.create_task(client.connect())
+    logging.info("SlurpBot is online.")
 
 
 @app.route("/submit_application", methods=["POST"])
@@ -105,15 +114,14 @@ async def submit_application():
     armory -- the link to wow armory ie. https://worldofwarcraft.com/en-us/character/us/{server}/{name}
     questions -- an array or questions and answers [{"q": "", "a": ""}, ...]
     """
-    print("submit_application post received")
+    logging.info("/submit_application post received.")
     payload = await request.get_json()
 
     # validate token
     if authenticate_request(payload) == False:
-        print(f"Invalid key: {payload['key']}")
+        logging.warning(f"Invalid key: {payload['key']}")
         return "Bad Request", 400
 
-    print("connecting to discord")
     guild = discord.utils.get(client.guilds, name=GUILD)
     applicant_category = discord.utils.get(
         (await guild.fetch_channels()), name="Applicants"
@@ -122,7 +130,7 @@ async def submit_application():
     channel = await create_applicant_channel(guild=guild, channel_name=channel_name)
 
     # embed important character details
-    await channel.send(embed=embed_application_response(data=payload))
+    await channel.send(embed=await embed_application_response(data=payload))
 
     # loop through the questions array
     for question_and_answer in payload["questions"]:
